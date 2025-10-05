e2e358b04a4df66a628642fc00448ae5dda41eb1
Hugoda <hhugodiaz23@gmail.com>
2025-10-05 17:16:55 -0600

    header actualizado a la ONG

--- laravel-app/resources/views/components/header.blade.php
+++ laravel-app/resources/views/components/header.blade.php
@@ -14,57 +14,56 @@
         </div>
       </div>
     </div><!-- End Top Bar -->
+<header id="header" class="header fixed-top">
+  <div class="branding d-flex align-items-center">
 
-    <div class="branding d-flex align-items-cente">
+    <div class="container position-relative d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- Logo de Habitat Guatemala -->
+        <img src="assets/img/habitat-logo.webp" alt="Habitat Guatemala" style="height:50px;">
+        <h1 class="sitename ms-2">Habitat Guatemala</h1>
+      </a>
 
-      <div class="container position-relative d-flex align-items-center justify-content-between">
-        <a href="index.html" class="logo d-flex align-items-center">
-          <!-- Uncomment the line below if you also wish to use an image logo -->
-          <!-- <img src="assets/img/logo.webp" alt=""> -->
-          <h1 class="sitename">Constructo</h1>
-        </a>
-
-        <nav id="navmenu" class="navmenu">
-          <ul>
-            <li><a href="users" class="active">Ingresar</a></li>
-            <li><a href="about">About</a></li>
-            <li><a href="services.html">Services</a></li>
-            <li><a href="projects.html">Projects</a></li>
-            <li><a href="team.html">Team</a></li>
-            <li class="dropdown"><a href="#"><span>More Pages</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-              <ul>
-                <li><a href="service-details.html">Service Details</a></li>
-                <li><a href="project-details.html">Project Details</a></li>
-                <li><a href="quote.html">Quote Form</a></li>
-                <li><a href="terms.html">Terms</a></li>
-                <li><a href="privacy.html">Privacy</a></li>
-                <li><a href="404.html">404</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-              <ul>
-                <li><a href="#">Dropdown 1</a></li>
-                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Dropdown 1</a></li>
-                    <li><a href="#">Deep Dropdown 2</a></li>
-                    <li><a href="#">Deep Dropdown 3</a></li>
-                    <li><a href="#">Deep Dropdown 4</a></li>
-                    <li><a href="#">Deep Dropdown 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Dropdown 2</a></li>
-                <li><a href="#">Dropdown 3</a></li>
-                <li><a href="#">Dropdown 4</a></li>
-              </ul>
-            </li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-          <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-        </nav>
-
-      </div>
-
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="index.html" class="active">Inicio</a></li>
+          <li><a href="about.html">Quiénes Somos</a></li>
+          <li><a href="programs.html">Programas</a></li>
+          <li><a href="projects.html">Proyectos</a></li>
+          <li><a href="team.html">Equipo</a></li>
+          <li><a href="users">Ingresar</a></li> 
+          <li class="dropdown"><a href="#"><span>Más Páginas</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="service-details.html">Detalles del Programa</a></li>
+              <li><a href="project-details.html">Detalles del Proyecto</a></li>
+              <li><a href="quote.html">Formulario de Donación</a></li>
+              <li><a href="terms.html">Términos</a></li>
+              <li><a href="privacy.html">Privacidad</a></li>
+              <li><a href="404.html">404</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a href="#"><span>Participa</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="#">Voluntariado</a></li>
+              <li class="dropdown"><a href="#"><span>Campañas</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Campaña 1</a></li>
+                  <li><a href="#">Campaña 2</a></li>
+                  <li><a href="#">Campaña 3</a></li>
+                  <li><a href="#">Campaña 4</a></li>
+                  <li><a href="#">Campaña 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Donaciones</a></li>
+              <li><a href="#">Eventos</a></li>
+              <li><a href="#">Alianzas</a></li>
+            </ul>
+          </li>
+          <li><a href="contact.html">Contacto</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
     </div>
 
-  </header>
+  </div>
+</header>
