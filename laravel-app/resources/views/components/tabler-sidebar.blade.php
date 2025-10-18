f955e7ca5e2e3b981256d75243c403c891b06575
224794 - JEFFERSON ALEXANDER PEREZ BLANCO <jperezb25@miumg.edu.gt>
2025-10-18 12:13:30 -0600

    reparacion de nav

--- laravel-app/resources/views/components/tabler-sidebar.blade.php
+++ laravel-app/resources/views/components/tabler-sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="navbar navbar-vertical navbar-expand-lg navbar-dark">
+<aside class="navbar navbar-vertical navbar-expand-md navbar-dark">
     <div class="w-100 flex">
         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#sidebar-menu" aria-controls="sidebar-menu" aria-expanded="true" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
